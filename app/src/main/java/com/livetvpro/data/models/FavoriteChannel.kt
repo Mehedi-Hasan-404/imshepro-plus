f586ba89c5c85c546951ab2cddbee3c6c3c54785
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-12-17 12:40:19 +0600

    Update FavoriteChannel.kt

--- app/src/main/java/com/livetvpro/data/models/FavoriteChannel.kt
+++ app/src/main/java/com/livetvpro/data/models/FavoriteChannel.kt
@@ -1,9 +1,13 @@
 package com.livetvpro.data.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class FavoriteChannel(
     val id: String,
     val name: String,
-    val url: String,      // This holds the M3U stream link
+    val url: String,      // The stream link must be saved here
     val logoUrl: String?,
-    val categoryName: String?
-)
+    val categoryName: String? = null
+) : Parcelable
