a0fce4cc8588464edc68002b5a40bb273fedc9a8
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-12-18 21:26:02 +0600

    Create TrackUiModel.kt

---
+++ app/src/main/java/com/livetvpro/ui/player/settings/TrackUiModel.kt
@@ -0,0 +1,28 @@
+sealed class TrackUiModel {
+    abstract val isSelected: Boolean
+
+    data class Video(
+        val groupIndex: Int,
+        val trackIndex: Int,
+        val width: Int,
+        val height: Int,
+        val bitrate: Int,
+        override val isSelected: Boolean
+    ) : TrackUiModel()
+
+    data class Audio(
+        val groupIndex: Int,
+        val trackIndex: Int,
+        val language: String,
+        val channels: Int,
+        val bitrate: Int,
+        override val isSelected: Boolean
+    ) : TrackUiModel()
+
+    data class Text(
+        val groupIndex: Int?,
+        val trackIndex: Int?,
+        val language: String,
+        override val isSelected: Boolean
+    ) : TrackUiModel()
+}
