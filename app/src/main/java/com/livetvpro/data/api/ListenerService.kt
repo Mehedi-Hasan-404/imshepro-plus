b4431f70028980e22023718c3942ba3fe6c057fd
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-12-26 08:46:20 +0600

    Create ListenerService.kt

---
+++ app/src/main/java/com/livetvpro/data/api/ListenerService.kt
@@ -0,0 +1,15 @@
+// app/src/main/java/com/livetvpro/data/api/ListenerService.kt
+package com.livetvpro.data.api
+
+import com.livetvpro.data.models.ListenerConfig
+import retrofit2.Response
+import retrofit2.http.GET
+
+/**
+ * API service for fetching listener configuration
+ */
+interface ListenerService {
+    
+    @GET("public/listener")
+    suspend fun getListenerConfig(): Response<ListenerConfig>
+}
