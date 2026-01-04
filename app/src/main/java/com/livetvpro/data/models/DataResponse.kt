8d30f3c818db89f78737938070223923e7afae8f
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-01-04 20:58:57 +0600

    Update DataResponse.kt

--- app/src/main/java/com/livetvpro/data/models/DataResponse.kt
+++ app/src/main/java/com/livetvpro/data/models/DataResponse.kt
@@ -2,6 +2,10 @@ package com.livetvpro.data.models
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Minimal data class - field names come from native code
+ * This is just for JSON parsing
+ */
 data class DataResponse(
     @SerializedName("categories")
     val categories: List<Category> = emptyList(),
