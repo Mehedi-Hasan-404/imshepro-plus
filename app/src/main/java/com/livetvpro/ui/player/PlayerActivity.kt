228976d816f09d075c0f2017c67fed4dbb7a8cbc
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-01-17 13:10:56 +0600

    Update PlayerActivity.kt

--- app/src/main/java/com/livetvpro/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/livetvpro/ui/player/PlayerActivity.kt
@@ -1,15 +1,20 @@
 package com.livetvpro.ui.player
 
+import android.app.PendingIntent
+import android.app.PictureInPictureParams
+import android.app.RemoteAction
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.res.Configuration
+import android.graphics.drawable.Icon
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.Parcelable
+import android.util.Rational
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
