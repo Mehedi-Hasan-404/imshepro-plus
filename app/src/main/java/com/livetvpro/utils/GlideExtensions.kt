97b7ede00d35896c5c568702fed9cec784fde4e6
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-02-02 23:13:09 +0600

    Create GlideExtensions.kt

---
+++ app/src/main/java/com/livetvpro/utils/GlideExtensions.kt
@@ -0,0 +1,90 @@
+package com.livetvpro.utils
+
+import android.graphics.drawable.PictureDrawable
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+
+/**
+ * Extension functions for Glide to handle both regular images and SVG files
+ */
+object GlideExtensions {
+    
+    /**
+     * Load image with automatic SVG detection and handling
+     */
+    fun loadImage(
+        imageView: ImageView,
+        url: String?,
+        placeholderResId: Int? = null,
+        errorResId: Int? = null,
+        isCircular: Boolean = false
+    ) {
+        if (url.isNullOrEmpty()) {
+            if (placeholderResId != null) {
+                imageView.setImageResource(placeholderResId)
+            }
+            return
+        }
+        
+        // Check if URL is SVG
+        if (url.endsWith(".svg", ignoreCase = true)) {
+            loadSvg(imageView, url, placeholderResId, errorResId, isCircular)
+        } else {
+            // Load regular image
+            var request = Glide.with(imageView.context)
+                .load(url)
+                .transition(DrawableTransitionOptions.withCrossFade())
+            
+            if (placeholderResId != null) {
+                request = request.placeholder(placeholderResId)
+            }
+            
+            if (errorResId != null) {
+                request = request.error(errorResId)
+            }
+            
+            if (isCircular) {
+                request = request.circleCrop()
+            }
+            
+            request.into(imageView)
+        }
+    }
+    
+    /**
+     * Load SVG image
+     */
+    private fun loadSvg(
+        imageView: ImageView,
+        url: String,
+        placeholderResId: Int?,
+        errorResId: Int?,
+        isCircular: Boolean
+    ) {
+        val requestBuilder: RequestBuilder<PictureDrawable> = Glide.with(imageView.context)
+            .`as`(PictureDrawable::class.java)
+            .listener(SvgSoftwareLayerSetter())
+        
+        var options = RequestOptions()
+        
+        if (placeholderResId != null) {
+            options = options.placeholder(placeholderResId)
+        }
+        
+        if (errorResId != null) {
+            options = options.error(errorResId)
+        }
+        
+        if (isCircular) {
+            options = options.circleCrop()
+        }
+        
+        requestBuilder
+            .apply(options)
+            .load(url)
+            .into(imageView)
+    }
+}
