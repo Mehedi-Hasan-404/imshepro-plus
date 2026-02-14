2179edbfe2fb83224d592964e378865ee3a4509a
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-02-14 23:48:18 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
-    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.android.application") version "8.7.3" apply false  // Updated to latest stable
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false  // Updated to Kotlin 2.1
+    id("com.google.dagger.hilt.android") version "2.52" apply false  // Updated
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false  // Updated for Kotlin 2.1
+    id("androidx.navigation.safeargs.kotlin") version "2.8.5" apply false  // Updated
+    id("com.google.gms.google-services") version "4.4.2" apply false  // Updated
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)  // Updated from buildDir (deprecated)
 }
