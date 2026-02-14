e6870eac299be91cd36f9e7504abebd6355401ff
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-02-14 14:26:40 +0600

    Update AppDatabase.kt

--- app/src/main/java/com/livetvpro/data/local/AppDatabase.kt
+++ app/src/main/java/com/livetvpro/data/local/AppDatabase.kt
@@ -4,15 +4,18 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.livetvpro.data.local.dao.FavoriteChannelDao
+import com.livetvpro.data.local.dao.PlaylistDao
 import com.livetvpro.data.local.entity.FavoriteChannelEntity
 import com.livetvpro.data.local.entity.FavoriteChannelConverters
+import com.livetvpro.data.local.entity.PlaylistEntity
 
 @Database(
-    entities = [FavoriteChannelEntity::class],
-    version = 3,
+    entities = [FavoriteChannelEntity::class, PlaylistEntity::class],
+    version = 4,  // Incremented version for new entity
     exportSchema = false
 )
 @TypeConverters(FavoriteChannelConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun favoriteChannelDao(): FavoriteChannelDao
+    abstract fun playlistDao(): PlaylistDao
 }
