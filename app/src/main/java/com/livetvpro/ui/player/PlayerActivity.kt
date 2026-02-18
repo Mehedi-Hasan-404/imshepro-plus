bfa503f5c3dcb51aae431dfdcf01797594fb286e
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-02-18 09:21:27 +0600

    Update PlayerActivity.kt

--- app/src/main/java/com/livetvpro/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/livetvpro/ui/player/PlayerActivity.kt
@@ -764,10 +764,12 @@ class PlayerActivity : AppCompatActivity() {
 
     private fun showSettingsDialog() {
         val exoPlayer = player ?: return
+        if (isFinishing || isDestroyed) return
         try {
             val dialog = com.livetvpro.ui.player.settings.PlayerSettingsDialog(this, exoPlayer)
             dialog.show()
         } catch (e: Exception) {
+            android.util.Log.e("PlayerActivity", "Error showing settings dialog", e)
         }
     }
 
