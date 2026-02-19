1ba8752ec40c0b41b37da648840631c8a6ce7261
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-02-19 11:52:23 +0600

    Update FloatingPlayerActivity.kt

--- app/src/main/java/com/livetvpro/ui/player/FloatingPlayerActivity.kt
+++ app/src/main/java/com/livetvpro/ui/player/FloatingPlayerActivity.kt
@@ -68,6 +68,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.livetvpro.ui.player.compose.PlayerControls
 import com.livetvpro.ui.player.compose.PlayerControlsState
