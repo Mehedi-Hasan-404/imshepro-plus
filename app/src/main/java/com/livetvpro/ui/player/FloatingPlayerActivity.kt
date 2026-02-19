1d0a8ed4a27e8ee1b24f0d20a889f09bd3e6d91a
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-02-20 05:40:50 +0600

    Update FloatingPlayerActivity.kt

--- app/src/main/java/com/livetvpro/ui/player/FloatingPlayerActivity.kt
+++ app/src/main/java/com/livetvpro/ui/player/FloatingPlayerActivity.kt
@@ -1722,8 +1722,12 @@ class FloatingPlayerActivity : AppCompatActivity() {
             val url = listenerManager.getMessageUrl()
             if (url.isNotBlank()) {
                 binding.tvMessageBanner.setOnClickListener {
-                    startActivity(android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse(url)))
+                    try {
+                        startActivity(android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse(url)))
+                    } catch (e: Exception) { }
                 }
+            } else {
+                binding.tvMessageBanner.setOnClickListener(null)
             }
         }
     }
