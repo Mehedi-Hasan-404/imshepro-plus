5bceff4a693119746bc25f1d0fb5ba663993e401
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-02-19 16:36:22 +0600

    Create FootballScoreFragment.kt

---
+++ app/src/main/java/com/livetvpro/ui/score/FootballScoreFragment.kt
@@ -0,0 +1,77 @@
+package com.livetvpro.ui.score
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.livetvpro.databinding.FragmentScoreWebviewBinding
+import com.livetvpro.utils.NativeListenerManager
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class FootballScoreFragment : Fragment() {
+
+    private var _binding: FragmentScoreWebviewBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var listenerManager: NativeListenerManager
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentScoreWebviewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val url = listenerManager.getFootLiveUrl()
+        if (url.isNotBlank()) {
+            setupWebView()
+            binding.webView.loadUrl(url)
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
+        binding.webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            loadWithOverviewMode = true
+            useWideViewPort = true
+            builtInZoomControls = true
+            displayZoomControls = false
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            userAgentString = "Mozilla/5.0 (Linux; Android 10; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
+        }
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                binding.progressBar.visibility = View.GONE
+            }
+        }
+        binding.webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView, newProgress: Int) {
+                if (newProgress < 100) {
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.progressBar.progress = newProgress
+                } else {
+                    binding.progressBar.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        binding.webView.destroy()
+        super.onDestroyView()
+        _binding = null
+    }
+}
