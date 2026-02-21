4692cb610a5078db6987426eaa9eedfa424d3e7b
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-02-21 12:58:51 +0600

    Update LiveTVProApplication.kt

--- app/src/main/java/com/livetvpro/LiveTVProApplication.kt
+++ app/src/main/java/com/livetvpro/LiveTVProApplication.kt
@@ -44,6 +44,9 @@ class LiveTVProApplication : Application() {
 
         applicationScope.launch {
             try {
+                // Try to restore cached data into native memory immediately (fast, no network)
+                dataRepository.refreshData()
+                // Then fetch fresh config in background for next time
                 dataRepository.fetchRemoteConfig()
             } catch (e: Exception) {
             }
